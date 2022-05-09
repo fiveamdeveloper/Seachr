9f54eb93a95a6b25940e34c3b751816dd9084087
Black Tiger <blacktiger@MacBook-Pro-von-Felix.fritz.box>
2022-05-09 22:06:52 +0200

    Implemented basic search gui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchResults from './SearchResults/SearchResults';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    var searchResults = [{ title: "Consilio GmbH", description: "Eine Beschreibung.", boost: 0.526, url: "https://www.google.com" }, { title: "Wiewald", description: "Eine Beschreibung.", boost: 0.481, url: "https://www.google.com" }]
+
+    return (
+        <div className='Container vh-100 mx-auto'>
+            <div className='d-flex vh-100 flex-column align-items-center justify-content-center'>
+                <div className='col-4 mb-1'>
+                    <h1 className='text-center'>Searchr</h1>
+                </div>
+                <div className='col-5 mb-3'>
+                    <div className='input-group'>
+                        <input className='form-control form-control-lg' placeholder='Bitte gib deinen Suchbegriff ein.' />
+                        <button className='btn btn-primary'>Finden</button>
+                    </div>
+                </div>
+
+                <div className='col-5'>
+                    <ul className="list-group">
+                        <SearchResults data={searchResults} />
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
